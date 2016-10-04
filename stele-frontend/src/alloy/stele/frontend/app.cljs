dfb052d8825b73e0ce08d7bf33f6e7c314ffe790
John Lutteringer <jlutteringer@credera.com>
2016-10-04 00:02:31 -0500

    more testing

--- stele-frontend/src/alloy/stele/frontend/app.cljs
+++ stele-frontend/src/alloy/stele/frontend/app.cljs
@@ -7,20 +7,22 @@
 	(:require-macros [mount.core :refer [defstate]]
 									 [reagent.ratom :refer [reaction]]))
 
+(defonce data (reagent/atom "Value"))
+
 (defn home-page []
 	[b/container
 	 [:div.row
 		[b/col
 		 [[:div.btn-toolbar
-			 [b/button "Primary"]
-			 [b/button [b/anchor "An Href" :uri "http://www.google.com"] :secondary]
+			 [b/button @data :type :secondary :outline true]
+			 [b/button [b/anchor "An fsdf" :uri "http://www.google.com"] :secondary]
 			 [b/button [b/anchor "An Href" :uri "http://www.google.com"] :style {:background-color "pink" :color "white"}]
-			 [:button {:class "btn btn-success" :type "button"} "Success"]
+			 [:button {:class "btn btn-success" :type "button"} "You Did It"]
 			 [:button {:class "btn btn-info" :type "button"} "Info"]
 			 [:button {:class "btn btn-warning" :type "button"} "Warning"]
 			 [:button {:class "btn btn-danger" :type "button"} "Danger"]
 			 [:button {:class "btn btn-link" :type "button"} "Link"]
-			 [:button {:class "btn" :style {:background-color "pink" :color "white"} :type "button"} "Jdub"]]]
+			 [:button {:class "btn" :style {:background-color "pink" :color "white"} :type "button"} "Test"]]]
 		 :size 9]
 		[b/col "Sidebar"]]])
 
